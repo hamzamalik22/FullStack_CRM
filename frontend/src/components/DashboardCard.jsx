b882c7297210657fee85be96743069f30117703d
hamzamalik22 <hamzamalikaj22@gmail.com>
2024-07-15 18:07:28 +0500

    Add Card Component and populate it

---
+++ frontend/src/components/DashboardCard.jsx
@@ -0,0 +1,76 @@
+import {
+  ChevronsUp,
+  DollarSign,
+  ChevronDown,
+  Users,
+  Logs,
+  NotepadTextDashed,
+} from "lucide-react";
+import React from "react";
+
+const iconMapping = {
+  dollar: DollarSign,
+  users: Users,
+  menu: Logs,
+  mail: NotepadTextDashed,
+  down: ChevronDown,
+  up: ChevronsUp,
+};
+
+const DashboardCard = ({ data }) => {
+  const {
+    title,
+    large_amount,
+    large_icon,
+    small_icon,
+    small_amount,
+    description,
+    progress,
+    color,
+  } = data;
+
+  const LargeIcon = iconMapping[large_icon];
+  const SmallIcon = iconMapping[small_icon];
+
+  return (
+    <>
+      <div className="outer relative w-[216px] h-[208px] rounded-[20px] bg-transparent border border-gray dark:border-zinc-400 text-zinc-900 dark:text-zinc-300 py-5 px-6 overflow-hidden">
+        <div className="inner-top flex items-center justify-between gap-8">
+          <div className="">
+            <h3 className="text-[13.5px] mt-4">{title}</h3>
+            <h1 className="text-3xl font-semibold mt-3">{large_amount}</h1>
+          </div>
+          <div
+            className={`h-14 w-14 mt-4 ${color} rounded-full flex-shrink-0`}
+          >
+            <span className="w-full h-full flex justify-center items-center text-white dark:text-black">
+              {LargeIcon && <LargeIcon size="18px" />}
+            </span>
+          </div>
+        </div>
+
+        <div className="inner-bottom mt-3 px-3 py-5 gap-2 flex justify-between items-center w-full ">
+          <div className="flex items-center text-xs text-green-400">
+            <span>{SmallIcon && <SmallIcon size="18px" />}</span>
+            <span>{small_amount && `${small_amount}%`}</span>
+          </div>
+          <div>
+            <p className="text-xs text-zinc-600 dark:text-zinc-300 text-nowrap">
+              {description}
+            </p>
+          </div>
+        </div>
+        {progress && (
+          <div className="w-full bg-gray-300 h-2 dark:bg-gray-700">
+            <div
+              className="bg-blue-600 h-2"
+              style={{ width: large_amount }}
+            ></div>
+          </div>
+        )}
+      </div>
+    </>
+  );
+};
+
+export default DashboardCard;
--- frontend/src/components/Overview.jsx
+++ frontend/src/components/Overview.jsx
@@ -1,10 +1,57 @@
 import React from "react";
+import DashboardCard from "./DashboardCard";
 
 const Overview = () => {
+  let listData = [
+    {
+      title: "BUDGET",
+      large_amount: "24k",
+      large_icon: "dollar",
+      small_icon: "up",
+      small_amount: "12",
+      description: "Since last month",
+      progress: "",
+      color: "bg-blue-500",
+    },
+    {
+      title: "CUSTOMERS",
+      large_amount: "$1.6k",
+      large_icon: "users",
+      small_icon: "down",
+      small_amount: "16",
+      description: "Since last month",
+      progress: "",
+      color: "bg-green-500",
+    },
+    {
+      title: "PROGRESS",
+      large_amount: "75.5%",
+      large_icon: "menu",
+      small_icon: "",
+      small_amount: "",
+      description: "",
+      progress: "progress_bar",
+      color: "bg-orange-500",
+    },
+    {
+      title: "PROFIT",
+      large_amount: "$15k",
+      large_icon: "mail",
+      small_icon: "",
+      small_amount: "",
+      description: "",
+      progress: "",
+      color: "bg-purple-500",
+    },
+  ];
   return (
     <>
-      <div className="h-full bg-green-300">
-        Overview
+      <div className="h-full p-4">
+        <div className="flex gap-5 flex-shrink-0">
+          {listData.map((item, index) => (
+            <DashboardCard key={index} data={item} />
+          ))}
+        </div>
       </div>
     </>
   );
